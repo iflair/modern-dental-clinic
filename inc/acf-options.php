cfa04b8ac9d10348fa2fb95f029581c073f2dda4
iFlair Web Technologies Pvt Ltd <jinal@iflair.com>
2025-12-02 16:51:41 +0530

    inc files updated as per new theme name

--- inc/acf-options.php
+++ inc/acf-options.php
@@ -11,8 +11,8 @@ if (function_exists('acf_add_options_page')) {
     
     // Main Clinic Settings Page
     acf_add_options_page(array(
-        'page_title'    => __('Clinic Settings', 'dental-care'),
-        'menu_title'    => __('Clinic Settings', 'dental-care'),
+        'page_title'    => __('Clinic Settings', 'modern-dental-clinic'),
+        'menu_title'    => __('Clinic Settings', 'modern-dental-clinic'),
         'menu_slug'     => 'mlt-clinic-settings',
         'capability'    => 'manage_options',
         'redirect'      => false,
@@ -23,25 +23,25 @@ if (function_exists('acf_add_options_page')) {
     // Register Clinic Settings Field Group
     acf_add_local_field_group(array(
         'key'           => 'group_mlt_clinic_settings',
-        'title'         => __('Clinic Settings', 'dental-care'),
+        'title'         => __('Clinic Settings', 'modern-dental-clinic'),
         'fields'        => array(
             array(
                 'key'           => 'field_mlt_clinic_logo',
-                'label'         => __('Clinic Logo', 'dental-care'),
+                'label'         => __('Clinic Logo', 'modern-dental-clinic'),
                 'name'          => 'mlt_clinic_logo',
                 'type'          => 'image',
                 'return_format' => 'url',
             ),
             array(
                 'key'           => 'field_mlt_clinic_tagline',
-                'label'         => __('Clinic Tagline', 'dental-care'),
+                'label'         => __('Clinic Tagline', 'modern-dental-clinic'),
                 'name'          => 'mlt_clinic_tagline',
                 'type'          => 'text',
                 'placeholder'   => 'Your smile is our priority',
             ),
             array(
                 'key'           => 'field_mlt_about_text',
-                'label'         => __('About the Clinic', 'dental-care'),
+                'label'         => __('About the Clinic', 'modern-dental-clinic'),
                 'name'          => 'mlt_about_text',
                 'type'          => 'wysiwyg',
                 'tabs'          => 'all',
@@ -49,13 +49,13 @@ if (function_exists('acf_add_options_page')) {
             ),
             array(
                 'key'           => 'field_mlt_clinic_email_alt',
-                'label'         => __('Alternative Email', 'dental-care'),
+                'label'         => __('Alternative Email', 'modern-dental-clinic'),
                 'name'          => 'mlt_clinic_email_alt',
                 'type'          => 'email',
             ),
             array(
                 'key'           => 'field_mlt_clinic_phone_alt',
-                'label'         => __('Alternative Phone', 'dental-care'),
+                'label'         => __('Alternative Phone', 'modern-dental-clinic'),
                 'name'          => 'mlt_clinic_phone_alt',
                 'type'          => 'text',
             ),
